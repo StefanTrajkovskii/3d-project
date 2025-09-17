a10947832e3c97712b8375877bdf8a49c187bd78
Stif <160525543+StefanTrajkovskii@users.noreply.github.com>
2025-09-17 08:30:05 +0200

    almost done

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
 import Header from "./components/Header" 
+import Hero from "./components/Hero"
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import { useEffect } from "react";
 
 
 export default function App() {
+
+  useEffect(() => {
+    AOS.init({
+      duration: 1500,
+      once: true,
+    });
+  }, []);
+
   return (
     <main>
       {/* Gradient Image */}
@@ -11,6 +23,7 @@ export default function App() {
       <div className="h-0 w-[40rem] absolute top-[20%] right-[5%] shadow-[0_0_900px_20px_#e99b63] -rotate-[30deg] z-10"></div>
 
       <Header/>
+      <Hero/>
 
     </main>
   )
